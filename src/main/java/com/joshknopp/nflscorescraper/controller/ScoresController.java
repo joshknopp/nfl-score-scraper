b1210444fcd40bd9e507d5b98f38d57d8f7552ee
joshknopp <github@joshknopp.com>
2021-03-16 02:23:24 +0000

    Minor

--- src/main/java/com/joshknopp/nflscorescraper/controller/ScoresController.java
+++ src/main/java/com/joshknopp/nflscorescraper/controller/ScoresController.java
@@ -1,15 +1,11 @@
 package com.joshknopp.nflscorescraper.controller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
 
 import com.joshknopp.nflscorescraper.service.ScraperService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/joshknopp/nflscorescraper/service/HtmlScoresServiceImpl.java
+++ src/main/java/com/joshknopp/nflscorescraper/service/HtmlScoresServiceImpl.java
@@ -1,15 +1,11 @@
 package com.joshknopp.nflscorescraper.service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 @Service
 public class HtmlScoresServiceImpl implements HtmlScoresService {
